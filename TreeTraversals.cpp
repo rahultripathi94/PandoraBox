9c322308531a490daccee29d1fea94106eacd0f5
rahultripathi94 <rahul.max94@gmail.com>
2021-05-25 19:59:27 +0530

    Test Commit

--- TreeTraversals.cpp
+++ TreeTraversals.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left, *right;
+    Node(int data){
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+void r_Preorder(struct Node *root){
+    if(root == NULL)
+        return;
+    else{
+        cout<<root->data<<" ";
+        r_Preorder(root->left);
+        r_Preorder(root->right);
+    }
+}
+
+int main(){
+    struct Node *root = new Node(10);
+    root->left = new Node(-5);
+    root->right = new Node(30);
+    root->left->left = new Node(-10);
+    root->left->right = new Node(0);
+    root->left->right->right = new Node(5);
+    root->right->right = new Node(36);
+
+    r_Preorder(root);
+}
+
 
